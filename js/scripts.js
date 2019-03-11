f4c8107c480bd62dc5cb4de8ac88dbb075d3ddcc
Devan <devy247@comcast.net>
2019-03-11 12:03:06 -0700

    light gallery functional

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,15 @@ console.log('OK');
 // Your Awesome Scripts
 
 // Light Gallery
-$('.gallery-1').lightGallery();
+$('#gallery-1').lightGallery({
+    thumbnail: false,
+    share: false,
+    mode: 'lg-tube'
+});
 
-$('.gallery-2').lightGallery();
+$('#gallery-2').lightGallery({
+    share:false,
+    download:false,
+    mode: 'lg-scale-up'
+});
 
